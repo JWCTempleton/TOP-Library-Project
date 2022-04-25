eb22ab572163e4ab4c87c96eb6965be152ea1fd0
Jacob Templeton <jwctempleton@gmail.com>
2022-04-24 20:11:47 -0700

    Add function to iterate through Library array and iterate through book objects and make div for each book

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(title, author, pages, wasRead) {
   this.title = title;
@@ -6,9 +6,9 @@ function Book(title, author, pages, wasRead) {
   this.pages = pages;
   this.wasRead = wasRead;
 
-  this.getInfo = function () {
-    return `${title} by ${author}, ${pages} pages, ${wasRead}.`;
-  };
+  // this.getInfo = function () {
+  //   return `${title} by ${author}, ${pages} pages, ${wasRead}.`;
+  // };
 }
 
 function addBookToLibrary(book) {
@@ -25,3 +25,25 @@ const book2 = new Book(
 );
 
 console.log(addBookToLibrary(book1));
+addBookToLibrary(book2);
+
+function addBook(book) {
+  const container = document.querySelector("#container");
+
+  // for (const book in myLibrary) {
+  //   div.textContent = `${book[title]}`;
+  // }
+  for (const book of myLibrary) {
+    const div = document.createElement("div");
+
+    for (const i in book) {
+      const p = document.createElement("p");
+      p.textContent += `${book[i]}\n`;
+      div.classList.add("card");
+      div.appendChild(p);
+    }
+    container.appendChild(div);
+  }
+}
+
+addBook(myLibrary);
